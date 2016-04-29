eaae5f7d55fde414fff9255cf181653e9779e806
Christopher Lam <chrislam67@gmail.com>
2016-04-29 17:53:45 -0500

    an example of how to validate json in the lambda, it is not good code or idiomatic, just an example

--- restApi/blog/post/handler.js
+++ restApi/blog/post/handler.js
@@ -3,6 +3,31 @@
 var doc = require('dynamodb-doc');
 var dynamo = new doc.DynamoDB();
 
+// all the ajv stuff is for json validation, this is not idiomatic or good, but just an example
+var Ajv = require('ajv');
+var ajv = Ajv({
+    allErrors: true
+});
+
+var schema = {
+    "required": [ "name", "age" ],
+    "properties": {
+        "name": {
+            "type": "string"
+        },
+        "age": {
+            "type": "number",
+            "maximum": 3
+        }
+    }
+};
+
+var validate = ajv.compile(schema);
+
+function isValidJson(data) {
+    return validate(data);
+}
+
 // Require Serverless ENV vars - this may or may not be needed I havent seen why it is
 var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
 
@@ -22,6 +47,11 @@ module.exports.handler = function(event, context) {
     switch (operation) {
         case 'create':
             console.log('debug', JSON.stringify(event.payload, null, 2));
+            console.log('debug 2', JSON.stringify(event.payload.Item.content, null, 2));
+
+            if (!isValidJson(event.payload.Item.content)) {
+                context.fail(new Error('Invalid Json' + event.payload.Item.content));
+            }
 
             var uuid = require('node-uuid');
             event.payload.Item.postId = uuid.v1();
